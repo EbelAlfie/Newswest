5107aba16b9561fa3ccff0fef5f439c24f900f72
Alfagift <alfagift@Alfagifts-MacBook-Pro.local>
2025-07-14 22:20:52 +0700

    First commit

---
+++ NewswestTests/NewswestTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewswestTests.swift
+//  NewswestTests
+//
+//  Created by Alfagift on 14/07/25.
+//
+
+import Testing
+@testable import Newswest
+
+struct NewswestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
